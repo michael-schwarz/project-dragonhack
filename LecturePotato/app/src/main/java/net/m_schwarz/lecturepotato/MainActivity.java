3c9559eb98ce1bee6e4a383423461e00bab54560
michael_schwarz <m.schwarz@tum.de>
2016-05-14 17:17:44 +0200

    Timer works now and keeps track of values within required interval

--- LecturePotato/app/src/main/java/net/m_schwarz/lecturepotato/MainActivity.java
+++ LecturePotato/app/src/main/java/net/m_schwarz/lecturepotato/MainActivity.java
@@ -1,22 +1,67 @@
 package net.m_schwarz.lecturepotato;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    ScreenReceiver mReceiver;
+    TimerTask timerTask;
+    Timer timer;
+    final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Register intents for getting screen on/off events
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_ON);
-        BroadcastReceiver mReceiver = new ScreenReceiver();
+        mReceiver = new ScreenReceiver();
         registerReceiver(mReceiver, filter);
+
+        TimePicker tp = (TimePicker) findViewById(R.id.lectureDurationPicker);
+        tp.setIs24HourView(true);
     }
+
+    public void startButtonClicked(View v){
+        TimePicker tp = (TimePicker) findViewById(R.id.lectureDurationPicker);
+        Date now = new Date();
+        Date end = new Date(now.getYear(),now.getMonth(),now.getDate(),tp.getCurrentHour(),tp.getCurrentMinute());
+        mReceiver.setEnd(end.getTime());
+
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mReceiver.over();
+                    }
+                });
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(timerTask,end.getTime()-System.currentTimeMillis());
+    }
+
+
 }
--- LecturePotato/app/src/main/java/net/m_schwarz/lecturepotato/ScreenReceiver.java
+++ LecturePotato/app/src/main/java/net/m_schwarz/lecturepotato/ScreenReceiver.java
@@ -3,19 +3,62 @@ package net.m_schwarz.lecturepotato;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.text.format.Time;
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by michael on 14.05.16.
  */
 public class ScreenReceiver extends BroadcastReceiver{
+    private long end;
+    private long lastOn;
+    private boolean on = true;
+    private long total;
+    private long active;
+
+    public void setEnd(long end){
+        on = true;
+        this.end = end;
+        this.total = end-System.currentTimeMillis();
+        this.lastOn = System.currentTimeMillis();
+        this.active = 0;
+    }
+
     @Override
     public void onReceive(Context arg0, Intent arg1){
         if (arg1.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-            Log.d("SCREEN","On at " + System.currentTimeMillis());
+            if(inTime()){
+                Log.v("SCREEN","On at " + System.currentTimeMillis());
+                lastOn = System.currentTimeMillis();
+            }
+            else {
+                Log.v("SCREEN","(Not recorded) On at " + System.currentTimeMillis());
+            }
+            on = true;
         }
         else if(arg1.getAction().equals(Intent.ACTION_SCREEN_OFF)){
-            Log.d("SCREEN","Off at " + System.currentTimeMillis());
+            if(inTime()){
+                Log.v("SCREEN","Off at " + System.currentTimeMillis());
+
+                active += (System.currentTimeMillis() - lastOn);
+            }
+            else {
+                Log.v("SCREEN","(Not recorded) Off at " + System.currentTimeMillis());
+            }
+
+            on = false;
         }
+    }
+
+    public void over(){
+        Log.v("TIME","Is up");
+        if(on) { active += (System.currentTimeMillis() - lastOn); }
+        Log.v("RESULT","Active " + active + " / " + total);
+    }
+
+    private boolean inTime(){
+        long current = System.currentTimeMillis();
+        return (current < end);
     }
 }
