e1c073d3688720ab48b79994cb4715a86c619347
User <User@NickMac.local>
2016-05-14 21:01:22 +0200

    Update sql

--- sql/add_all_functions.sql
+++ sql/add_all_functions.sql
@@ -12,7 +12,7 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION dh.add_user(nickname char[20], device_id uuid, university_id integer, name char[100], country char[100]) RETURNS void AS $$
 BEGIN
-	INSETR INTO dh.users ( nickname, device_id, university_id,
+	INSETR INTO dh.users ( nickname, device_id, university_id)
 	VALUES (nickname, device_id, university_id)
 
 	INSERT INTO dh.universities (university_id, name, country)
