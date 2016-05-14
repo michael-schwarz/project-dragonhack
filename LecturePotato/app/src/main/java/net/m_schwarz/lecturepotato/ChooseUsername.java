fae1b39742129d4fca1d40d678c620a50adb68bb
michael_schwarz <m.schwarz@tum.de>
2016-05-14 21:09:03 +0200

    Started prep for network by making calls async

--- LecturePotato/app/src/main/java/net/m_schwarz/lecturepotato/ChooseUsername.java
+++ LecturePotato/app/src/main/java/net/m_schwarz/lecturepotato/ChooseUsername.java
@@ -2,6 +2,7 @@ package net.m_schwarz.lecturepotato;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -49,14 +50,39 @@ public class ChooseUsername extends AppCompatActivity {
         EditText et = (EditText) findViewById(R.id.username);
         String username = et.getText().toString();
 
-        if(Users.existsUser(username)){
+        AsyncTask<String,Void,Boolean> checkUsernameTask = new AsyncTask<String, Void, Boolean>() {
+            @Override
+            protected Boolean doInBackground(String... params) {
+                try {
+                    return Users.existsUser(params[0]);
+                } catch (Exception e) {
+                    //TODO:
+                    e.printStackTrace();
+                    return false;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                existsUser(result);
+            }
+        };
+
+        checkUsernameTask.execute(username);
+    }
+
+    void existsUser(boolean result){
+        if(result){
             Toast.makeText(ChooseUsername.this, "Nope", Toast.LENGTH_SHORT).show();
         }
         else {
+            EditText et = (EditText) findViewById(R.id.username);
+            String username = et.getText().toString();
+
             Users.createUser(getDeviceId(),username,uni);
             Intent intent = new Intent(this,MainActivity.class);
             startActivity(intent);
-         //   finish();
+            finish();
         }
     }
 
